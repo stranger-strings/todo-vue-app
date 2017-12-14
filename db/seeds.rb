bb68e56b2673584dcb6e106419d49791b1da7a75
Peter Jang <peter.x.jang@gmail.com>
2017-12-14 14:29:06 -0600

    Add task model with seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create(text: "Take out the garbage", completed: false)
+Task.create(text: "Make the bed", completed: false)
+Task.create(text: "Mow the lawn", completed: false)
+Task.create(text: "Buy groceries", completed: false)
+Task.create(text: "Feed the cat", completed: false)
